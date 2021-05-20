b9712e229ec28be4c35ddac7ae14c09a7a029acf
Artur Barseghyan <artur.barseghyan@gmail.com>
2021-05-20 23:19:06 +0200

    Prepare 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import setup, find_packages
 
-version = "0.1.3"
+version = "0.1.4"
 
 try:
     readme = open(os.path.join(os.path.dirname(__file__), "README.rst")).read()
